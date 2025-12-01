e281be168987450e39bef9d0d22bb7d34b622905
𝒮𝑒𝓋𝒾𝓃𝒸𝒽 <ysevinchoy2197@gmail.com>
2025-12-01 15:34:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+#1-misol
+n = "transportation"
+a = n[:8]
+print(a)
+
+#2-misol
+n = "demonstration"
+a = n[5:10]
+print(a)
+
+#3-misol
+n = "2468013579"
+a = n[-5:]
+print(a)
+
+#4-misol
+n = "environment"
+a = n[3:7]
+print(a)
+
+#5-misol
+n = "strawberry"
+a = n[-3:]
+print(a)
+
+#6-misol
+n = "prgrammer"
+a = n[2:6]
+print(a)
+
+#7-misol
+n = "abcdefghijklmnop"
+a = n[3:7]
+print(a)
+
+#8-misol
+n = "ZXCVBNMASDFGHJ"
+a = n[:]
+print(a)
